0204859aa049203c8f25aa41ca2e2aef5ad5d01c
Angelika Brown <angelikpbrown@gmail.com>
2026-07-24 16:03:09 -0400

    Initialize React and FastAPI applications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("Loading...");
 
   useEffect(() => {
     fetch("http://localhost:8000/")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      })
+      .catch((err) => console.error(err));
   }, []);
 
   return <h1>{message}</h1>;
