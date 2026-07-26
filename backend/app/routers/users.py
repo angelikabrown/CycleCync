e182a3b8ff59c54145c5d4079a1a6481cdabdf8b
Angelika Brown <angelikpbrown@gmail.com>
2026-07-26 17:45:24 -0400

    creating a new user

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.user import User
 from app.schemas.user import UserCreate
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/register")
-def register_user(user: UserCreate):
+def register_user(user: UserCreate,
+                  db: Session = Depends(get_db)):
+    
+    #create a new user instance
+    new_user = User(username=user.username, email=user.email, password=user.password)
 
     return {
         "message": "User received",
