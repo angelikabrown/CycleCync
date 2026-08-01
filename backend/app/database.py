2ab6b4235a16b1e80731afc67a898f93acb354fa
Angelika Brown <angelikpbrown@gmail.com>
2026-08-01 11:56:38 -0400

    daily_check_in successfully in the db

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,8 +3,7 @@ import os
 from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Load environment variables from .env
 load_dotenv()
