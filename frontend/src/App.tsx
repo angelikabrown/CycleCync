c660334317e8d7e2521f6bc79ab1f3704a518420
Angelika Brown <angelikpbrown@gmail.com>
2026-09-21 11:53:12 -0400

    testing login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import CheckInHistory from "./components/CheckInHistory";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
       <h1>Hello CycleCync</h1>
+      <Login />
       <CheckInHistory />
     </div>
   );
 }
+
 
 export default App;
